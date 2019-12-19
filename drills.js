3495979f2b434a8a20fad5edb6dc33b8134e2bde
balayaydemir <balayaydemir@gmail.com>
2019-12-19 12:46:16 -0500

    done with #1

---
+++ drills.js
@@ -0,0 +1,18 @@
+/**
+ * #1
+ * 
+ * Given the following list of numbers 21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40
+
+What is the resulting list that will be sorted after 3 recursive calls to mergesort?
+What is the resulting list that will be sorted after 16 recursive calls to mergesort?
+What are the first 2 lists to be merged?
+Which two lists would be merged on the 7th merge?
+ 
+
+21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40
+
+a) [21]
+b) [2]
+c) [1, 21]
+d) [28, 29], [2, 9]
+ */
